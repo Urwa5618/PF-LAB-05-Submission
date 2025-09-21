4b03d5d6bbb10a7b77bd778da2120bc5745c2ebb
Hafiz Urwa Rafique <k253066@nu.edu.pk>
2025-09-21 12:10:49 +0500

    Delete 25K-3066 Lab 5 PF LAB Question 6.cpp

--- 25K-3066 Lab 5 PF LAB Question 6.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int a,b,Max;
-	printf("Enter value of a, Enter value of b");
-	scanf("%d %d",&a, &b);
-	Max= (a>b)? a : b;
-	printf("the greater is %d", Max);
-	return 0;
-}
